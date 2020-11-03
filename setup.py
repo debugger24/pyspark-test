162a28e74a2c8169188eb00f90657b7a0b761dbb
Rahul Kumar <rahulcomp24@gmail.com>
2020-11-04 02:39:56 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,15 +8,15 @@ def readme():
 
 setup(
     name="pyspark_test",
-    version="0.0.2",
-    description="",
+    version="0.1.0",
+    description="Check that left and right spark DataFrame are equal.",
     long_description=readme(),
     long_description_content_type="text/markdown",
     url="https://github.com/debugger24/pyspark-test",
     author="Rahul Kumar",
     author_email="rahulcomp24@gmail.com",
     keywords="assert pyspark unit test testing compare",
-    license="Apache Software License",
+    license="Apache Software License (Apache 2.0)",
     py_modules=["pyspark_test"],
     package_dir={"": "src"},
     install_requires=["pyspark>=2.1.2"],
